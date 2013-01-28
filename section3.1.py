6f7719aecae7de5b350253d3b9d0f2964fa408d3
Jan Gosmann <jan@hyper-world.de>
2013-01-28 11:04:52 +0100

    More metrics.

--- section3.1.py
+++ section3.1.py
@@ -9,6 +9,7 @@ import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import quantities as pq
 import scipy as sp
+import spykeutils.tools as stools
 import spykeutils.spike_train_generation as stg
 import spykeutils.spike_train_metrics as stm
 
@@ -29,11 +30,31 @@ config_spec = config.ConfigSpec({
     })
 })
 
+
+def binning_distance(trains, tau):
+    sampling_rate = 1.0 / tau
+    binned, dummy = stools.bin_spike_trains({0: trains}, sampling_rate)
+    binned = sp.tile(sp.vstack(trains), (len(trains), 1, 1))
+    return sp.sum((binned - sp.rollaxis(binned, 1)) ** 2, axis=2)
+
+
+def normalized_vp_dist(trains, tau):
+    num_spikes = sp.atleast_2d(sp.asarray([st.size for st in trains]))
+    return stm.victor_purpura_dist(
+        trains, 2.0 / tau, sort=False) / (num_spikes + num_spikes.T)
+
+
 metrics = {
-    'vp': lambda trains, tau: stm.victor_purpura_dist(
+    'D_V': lambda trains, tau: stm.victor_purpura_dist(
         trains, 2.0 / tau, sort=False),
-    'vr': lambda trains, tau: stm.van_rossum_dist(trains, tau, sort=False)
+    'D_V*': normalized_vp_dist,
+    'D_R': lambda trains, tau: stm.van_rossum_dist(trains, tau, sort=False),
+    'D_B': binning_distance,
+    'D_ES': lambda trains, tau: stm.event_synchronization(
+        trains, tau, sort=False),
+    'D_HM': stm.hunter_milton_similarity
 }
+
 
 with open("conf/testing.conf") as config_file:
     cfg = config.load(config_spec, config_file)[name]
@@ -55,24 +76,31 @@ def gen_trains():
 # TODO split this function, so that we have one call per metric and tau, this
 # allows to reuse more cached results.
 @memory.cache
-def calc_metric(trains_by_rate):
-    logger.info("Calculating metric")
+def calc_single_metric(trains_by_rate, metric, tau):
+    result = sp.empty((cfg['evaluation_points'], cfg['evaluation_points']))
+    dist_mat = metric(list(itertools.chain(*trains_by_rate)), tau)
+    for i in xrange(cfg['evaluation_points']):
+        i_start = cfg['spike_trains_per_rate'] * i
+        i_stop = i_start + cfg['spike_trains_per_rate']
+        for j in xrange(i, cfg['evaluation_points']):
+            j_start = cfg['spike_trains_per_rate'] * j
+            j_stop = j_start + cfg['spike_trains_per_rate']
+            result[i, j] = result[j, i] = sp.mean(
+                dist_mat[i_start:i_stop, j_start:j_stop])
+    return result
+
+
+def calc_metrics(trains_by_rate):
+    logger.info("Calculating metrics")
     results = sp.empty((
         len(cfg['metrics']), cfg['time_scales'].size, cfg['evaluation_points'],
         cfg['evaluation_points']))
     for m, metric in enumerate(cfg['metrics']):
+        logger.info("  metric %s" % metric)
         for t, tau in enumerate(cfg['time_scales']):
-            logger.info("  for time scale %s" % str(tau))
-            dist_mat = metrics[metric](
-                list(itertools.chain(*trains_by_rate)), tau)
-            for i in xrange(cfg['evaluation_points']):
-                i_start = cfg['spike_trains_per_rate'] * i
-                i_stop = i_start + cfg['spike_trains_per_rate']
-                for j in xrange(i, cfg['evaluation_points']):
-                    j_start = cfg['spike_trains_per_rate'] * j
-                    j_stop = j_start + cfg['spike_trains_per_rate']
-                    results[m, t, i, j] = results[m, t, j, i] = sp.mean(
-                        dist_mat[i_start:i_stop, j_start:j_stop])
+            logger.info("    for time scale %s" % str(tau))
+            results[m, t, :, :] = calc_single_metric(
+                trains_by_rate, metrics[metric], tau)
     return results
 
 
@@ -94,5 +122,5 @@ def plot(results):
             plt.colorbar()
 
 logger.info("Section 3.1")
-plot(calc_metric(gen_trains()))
+plot(calc_metrics(gen_trains()))
 plt.show()
