994c6b2157b2c3c4063834bb56201e4dd27cf3e6
Jan Gosmann <jan@hyper-world.de>
2013-01-29 10:19:05 +0100

    Extract metrics stuff to own module.

--- section3.1.py
+++ section3.1.py
@@ -2,6 +2,7 @@
 
 from __future__ import division
 from joblib import Memory, Parallel, delayed
+from metrics import metrics
 import argparse
 import config
 import itertools
@@ -18,31 +19,6 @@ name = 'section3.1'
 memory = Memory(cachedir='cache', verbose=1)
 logging.basicConfig()
 logger = logging.getLogger(name)
-
-
-def binning_distance(trains, tau):
-    sampling_rate = 1.0 / tau
-    binned, dummy = stools.bin_spike_trains({0: trains}, sampling_rate)
-    binned = sp.tile(sp.vstack(binned[0]), (len(trains), 1, 1))
-    return sp.sum((binned - sp.rollaxis(binned, 1)) ** 2, axis=2)
-
-
-def normalized_vp_dist(trains, tau):
-    num_spikes = sp.atleast_2d(sp.asarray([st.size for st in trains]))
-    return stm.victor_purpura_dist(
-        trains, 2.0 / tau, sort=False) / (num_spikes + num_spikes.T)
-
-
-metrics = {
-    'D_V': lambda trains, tau: stm.victor_purpura_dist(
-        trains, 2.0 / tau, sort=False),
-    'D_V^*': normalized_vp_dist,
-    'D_R': lambda trains, tau: stm.van_rossum_dist(trains, tau, sort=False),
-    'D_B': binning_distance,
-    'D_{ES}': lambda trains, tau: stm.event_synchronization(
-        trains, tau, sort=False),
-    'D_{HM}': stm.hunter_milton_similarity
-}
 
 
 @memory.cache
