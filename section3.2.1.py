4087fca7496b344c6b1da7134921aef807a8bfcc
Jan Gosmann <jan@hyper-world.de>
2013-02-01 15:09:18 +0100

    Documenting section 3.2.1 code.

--- section3.2.1.py
+++ section3.2.1.py
@@ -22,12 +22,19 @@ logger = logging.getLogger(name)
 
 
 def gen_single_trial(interval_length, rates):
+    """ Generates a single spike train with intervals of length
+    `interval_length` and the firing rates given in `rates`.
+    """
     return stools.st_concatenate([stg.gen_homogeneous_poisson(
         rate, t_start=i * interval_length,
         t_stop=(i + 1) * interval_length) for i, rate in enumerate(rates)])
 
 
 def gen_trains(interval_length, rates, num_trials, num_repetitions):
+    """ Generates a list of `num_repititions` lists of `num_trials` spike
+    trains. Each spike train will have intervals with length `interval_length`
+    and the firing rates given in `rates`.
+    """
     return [[gen_single_trial(interval_length, rates)
             for j in xrange(num_trials)] for i in xrange(num_repetitions)]
 
@@ -35,6 +42,10 @@ def gen_trains(interval_length, rates, num_trials, num_repetitions):
 @memory.cache
 def gen_trains_pair(
         interval_length, rates_a, rates_b, num_trials, num_repetitions):
+    """ Calls :func:`gen_trains` twice with `rates` set to `rates_a` and
+    `rates_b` once. All other parameters passed to this function directly
+    correspond to :func:`gen_trains`.
+    """
     logger.info("Generating spike trains")
     return gen_trains(interval_length, rates_a, num_trials, num_repetitions), \
         gen_trains(interval_length, rates_b, num_trials, num_repetitions)
@@ -42,12 +53,34 @@ def gen_trains_pair(
 
 @memory.cache
 def calc_single_metric(trains, metric, tau):
+    """ Calculates a metric on spike trains.
+
+    :param list trains: Spike trains to calculate the metric on.
+    :param str metric: The metric to calculate. Has to be a key in
+        :const:`metrics.metrics`.
+    :param tau: Time scale parameter for the metric.
+    :type tau: Quantity scalar.
+    :returns: The metric for all pairs of spike trains.
+    :rtype: 2-D array
+    """
     logger.info("Calculating metric %s for time_scale %s." % (metric, str(tau)))
     return metrics[metric](trains, tau)
 
 
 @memory.cache
 def calc_probability_matrix(trains_a, trains_b, metric, tau, z):
+    """ Calculates the probability matrix that one spike train from stimulus X
+    will be classified as spike train from stimulus Y.
+
+    :param list trains_a: Spike trains of stimulus A.
+    :param list trains_b: Spike trains of stimulus B.
+    :param str metric: Metric to base the classification on. Has to be a key in
+        :const:`metrics.metrics`.
+    :param tau: Time scale parameter for the metric.
+    :type tau: Quantity scalar.
+    :param float z: Exponent parameter for the classifier.
+    """
+
     with warnings.catch_warnings():
         warnings.filterwarnings("ignore", "divide by zero")
         dist_mat = calc_single_metric(
@@ -66,6 +99,12 @@ def calc_probability_matrix(trains_a, trains_b, metric, tau, z):
 
 
 def calc_mutual_information(probability_mat):
+    """ Calculates the mutual information of stimulus and predicted stimulus
+    from a probability matrix.
+
+    :rtype: float
+    """
+
     marginals = sp.outer(
         sp.sum(probability_mat, axis=1), sp.sum(probability_mat, axis=0))
     p = probability_mat[probability_mat != 0.0]
@@ -74,15 +113,43 @@ def calc_mutual_information(probability_mat):
 
 
 def calc_stimuli_entropy():
+    """ :returns: Stimulus entropy for setting with 2 different stimuli of equal
+        probability.
+    """
     return -2.0 * 0.5 * sp.log(0.5)
 
 
 def calc_uncertainty_reduction(trains_a, trains_b, metric, z, tau):
+    """ Returns the percentage of uncertainty reduction by the stimulus
+    prediction.
+
+    :param list trains_a: Spike trains of stimulus A.
+    :param list trains_b: Spike trains of stimulus B.
+    :param str metric: Metric to base the classification of stimuli on for the
+        calculation. Has to be a key in :const:`metrics.metrics`.
+    :param z: Exponent parameter for the classifier.
+    :param tau: Time scale parameter for the metric.
+    :type tau: Quantity scalar.
+    :returns: The percentage of uncertainty reduction.
+    :rtype: float
+    """
+
     return calc_mutual_information(calc_probability_matrix(
         trains_a, trains_b, metric, tau, z)) / calc_stimuli_entropy()
 
 
 def run_single_experiment(cfg, experiment_idx, n_jobs=1):
+    """ Run `experiment_idx`-th experiment defined in the configuration `cfg`
+    with `n_jobs` parallel processes.
+
+    :returns: Tuple with:
+        * The percentages of uncertainty reduction as an array of the shape
+          (metrics, z-values, tau-values, repetitions of the experiment)
+        * The percentages of uncertainty reduction for `tau = inf` as an array
+          of the shape (metrics, z-values, repetitions of the experiment)
+    :rtype: (4-D array, 3-D array)
+    """
+
     exp_cfg = cfg['experiments'][experiment_idx]
     trains_a, trains_b = gen_trains_pair(
         cfg['interval_length'], exp_cfg['rates_a'], exp_cfg['rates_b'],
@@ -110,12 +177,28 @@ def run_single_experiment(cfg, experiment_idx, n_jobs=1):
 
 
 def run_experiments(cfg, n_jobs=1):
+    """ Run all experiments defined in the configuration `cfg` with `n_jobs`
+    parallel processes.
+
+    :returns: Tuple with:
+        * List of the percentages of uncertainty reduction as an array of the
+          shape (metrics, z-values, tau-values, repetitions of the experiment)
+        * List of the percentages of uncertainty reduction for `tau = inf` as
+          an array of the shape (metrics, z-values, repetitions of the
+          experiment)
+    :rtype: (list of 4-D arrays, list of 3-D arrays)
+    """
+
     return zip(
         *[run_single_experiment(cfg, i, n_jobs)
           for i in xrange(len(cfg['experiments']))])
 
 
 def plot_stparams(interval_length, rates, color):
+    """ Plot a spike train consisting of intervals of the length
+    `interval_length` with firing rates given in `rates` in the color `color`.
+    """
+
     for i, rate in enumerate(rates):
         plt.plot(
             [i * interval_length, (i + 1) * interval_length],
@@ -123,6 +206,17 @@ def plot_stparams(interval_length, rates, color):
 
 
 def plot_uncertainty_reduction(cfg, results, results_inftau):
+    """ Plot the percentage of reduction of uncertainty.
+
+    :param dict cfg: Configuration.
+    :param list results: List of results with one element per experiment. Each
+        element has to be a 4-D array of the shape (metrics, z-values,
+        tau-values, experimental repetitions).
+    :param list result_list_inftau: List of results for `tau = inf` with one
+        element per experiment. Each element has to be a 3-D array of the shape
+        (metrics, z-values, experimental repetitions).
+    """
+
     z_colors = ['k', 'r', 'b', 'g']
     for e, experiment in enumerate(cfg['experiments']):
         plt.subplot(len(cfg['metrics']) + 1, len(cfg['experiments']), e + 1)
@@ -154,6 +248,17 @@ def plot_uncertainty_reduction(cfg, results, results_inftau):
 
 
 def plot_param_per_metric_and_z(values, err=None, c=None):
+    """ Plots values on the y-axis sorted by metric and z-value on the x-axis.
+
+    :param values: Array with shape (metrics, z-values) with values to plot.
+    :type values: 2-D array
+    :param err: Array with shape (metrics, z-values) or (metrics, z-values, 2)
+        denoting the error bars to plot.
+    :type err: 2-D or 3-D array
+    :param c: Color to plot in. If `None`, blue and red will be used alternating
+        for each metric.
+    """
+
     color = 'r'
     for m, metric in enumerate(cfg['metrics']):
         color = 'b' if color == 'r' else 'r'
@@ -168,6 +273,15 @@ def plot_param_per_metric_and_z(values, err=None, c=None):
 
 
 def plot_bool_indicator_per_metric_and_z(values, c='g'):
+    """ Plots markers on top of the y-axis sorted by metric and z-value on the
+    x-axis.
+
+    :param values: Boolean array with shape (metrics, z-values) indicating
+        where to plot markers.
+    :type values: 2-D array
+    :param c: Color to plot in.
+    """
+
     for m, metric in enumerate(cfg['metrics']):
         for z in xrange(len(cfg['zs'])):
             x = m * len(cfg['zs']) + z + 0.5
@@ -176,6 +290,15 @@ def plot_bool_indicator_per_metric_and_z(values, c='g'):
 
 
 def plot_optimal_uncertainty_reduction(results_for_exp, results_for_exp_inftau):
+    """ Plot the percentage of uncertainty reduction of the optimal classifiers.
+
+    :param results_for_exp: The results of one experiment as 4-D array of the
+        shape (metrics, z-values, tau-values, experimental repetitions).
+    :type results_for_exp: 4-D array
+    :param result_list_inftau: The results of one experiment for `tau = inf` as
+        3-D array of the shape (metrics, z-values, experimental repetitions).
+    :type results_for_exp_inftau: 3-D array.
+    """
     plt.ylim(0, 1)
 
     plot_param_per_metric_and_z(
@@ -185,6 +308,15 @@ def plot_optimal_uncertainty_reduction(results_for_exp, results_for_exp_inftau):
 
 
 def plot_optimal_tau(results_for_exp, results_for_exp_inftau):
+    """ Plot the mean of optimal taus.
+
+    :param results_for_exp: The results of one experiment as 4-D array of the
+        shape (metrics, z-values, tau-values, experimental repetitions).
+    :type results_for_exp: 4-D array
+    :param result_list_inftau: The results of one experiment for `tau = inf` as
+        3-D array of the shape (metrics, z-values, experimental repetitions).
+    :type results_for_exp_inftau: 3-D array.
+    """
     values = sp.empty((results_for_exp.shape[0], results_for_exp.shape[1]))
     err = sp.empty((results_for_exp.shape[0], results_for_exp.shape[1], 2, 1))
     num_trials = results_for_exp.shape[3]
@@ -203,6 +335,15 @@ def plot_optimal_tau(results_for_exp, results_for_exp_inftau):
 
 def plot_optimal_tau_for_mean_uncertainty_reduction(
         results_for_exp, results_for_exp_inftau):
+    """ Plot the optimal tau for the mean of uncertainty reduction.
+
+    :param results_for_exp: The results of one experiment as 4-D array of the
+        shape (metrics, z-values, tau-values, experimental repetitions).
+    :type results_for_exp: 4-D array
+    :param result_list_inftau: The results of one experiment for `tau = inf` as
+        3-D array of the shape (metrics, z-values, experimental repetitions).
+    :type results_for_exp_inftau: 3-D array.
+    """
     values = sp.empty((results_for_exp.shape[0], results_for_exp.shape[1]))
     err = sp.empty((results_for_exp.shape[0], results_for_exp.shape[1], 2, 1))
     mark = sp.empty((results_for_exp.shape[0], results_for_exp.shape[1]))
@@ -220,6 +361,17 @@ def plot_optimal_tau_for_mean_uncertainty_reduction(
 
 
 def plot_optima(cfg, results, results_inftau):
+    """ Plot optima for uncertainty reduction and tau values.
+
+    :param dict cfg: Configuration.
+    :param list results: List of results with one element per experiment. Each
+        element has to be a 4-D array of the shape (metrics, z-values,
+        tau-values, experimental repetitions).
+    :param list result_list_inftau: List of results for `tau = inf` with one
+        element per experiment. Each element has to be a 3-D array of the shape
+        (metrics, z-values, experimental repetitions).
+    """
+
     for e, experiment in enumerate(cfg['experiments']):
             plt.subplot(
                 3, len(cfg['experiments']), 0 * len(cfg['experiments']) + e + 1)
