c7f3561cc5fd9749d5b70886d7e6f1073195f751
Jan Gosmann <jan@hyper-world.de>
2013-02-11 13:26:26 +0100

    Adjust ranges

--- spykeutils/tests/profile_spike_train_metrics.py
+++ spykeutils/tests/profile_spike_train_metrics.py
@@ -136,7 +136,7 @@ if __name__ == '__main__':
             data = pickle.load(f)
         print "Loaded stored benchmarking data."
     except:
-        data = BenchmarkData(sp.arange(1, 210, 10), sp.arange(1, 11))
+        data = BenchmarkData(sp.arange(10, 210, 10), sp.arange(2, 11))
         if args.data is not None:
             with open(args.data[0], 'w') as f:
                 pickle.dump(data, f)
