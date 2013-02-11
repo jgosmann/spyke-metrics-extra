6a156713cf2a40ee8d9619439d07660ee44f3561
Jan Gosmann <jan@hyper-world.de>
2013-02-11 12:47:07 +0100

    Improve plot.

--- spykeutils/tests/profile_spike_train_metrics.py
+++ spykeutils/tests/profile_spike_train_metrics.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 
 import argparse
-import cProfile
-import os
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
 import pickle
 import pstats
 import quantities as pq
+import scipy as sp
 import spykeutils.signal_processing as sigproc
 import spykeutils.spike_train_generation as stg
 import spykeutils.spike_train_metrics as stm
 import sys
-import tempfile
+import timeit
 
 
 tau = 5.0 * pq.ms
@@ -64,51 +65,83 @@ def profile_metrics(trains, to_profile):
         metrics[key][1](trains)
 
 
+class BenchmarkData(object):
+    def __init__(
+            self, spike_count_range, train_count_range, firing_rate=50 * pq.Hz):
+        self.spike_count_range = spike_count_range
+        self.train_count_range = train_count_range
+        self.num_trains_per_spike_count = sp.amax(train_count_range)
+        self.trains = [
+            [stg.gen_homogeneous_poisson(firing_rate, max_spikes=num_spikes)
+             for i in xrange(self.num_trains_per_spike_count)]
+            for num_spikes in spike_count_range]
+
+
+class Benchmark(object):
+    def __init__(self, benchmark_data, repeats=10):
+        self.repeats = repeats
+        self.data = benchmark_data
+
+    def benchmark_metric(self, metric):
+        times = sp.empty((
+            len(self.data.spike_count_range), len(self.data.train_count_range)))
+        for i, j in sp.ndindex(*times.shape):
+            trains = self.data.trains[i][:self.data.train_count_range[j]]
+            assert trains is not None  # Suppress pyflakes unused warning
+            times[i, j] = timeit.timeit(lambda: metric(trains), number=self.repeats)
+        return times
+
+
+def plot_benchmark(times, data):
+    left = data.train_count_range[0]
+    right = data.train_count_range[-1]
+    bottom = data.spike_count_range[0]
+    top = data.spike_count_range[-1]
+    plt.imshow(
+        times, origin='lower', aspect='auto', extent=(left, right, bottom, top),
+        cmap=cm.get_cmap('hot'))
+    plt.xlabel("# spike trains")
+    plt.ylabel("# spikes per spike trains")
+    cb = plt.colorbar()
+    cb.set_label("Calculation time / s")
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="Profile the spike train distances.")
     parser.add_argument(
-        '--trains', '-t', type=str, nargs=1,
-        help="Use given file to load spike trains. If not existent, spike " +
-        "trains will be generated as usual and saved in this file.")
+        '--data', '-d', type=str, nargs=1,
+        help="Use given file to load spike trains for benchmarking. If not " +
+        "existent, spike trains will be generated as usual and saved in this " +
+        "file.")
     parser.add_argument(
-        '--distances', '-d', type=str, nargs='*', default=metrics.iterkeys(),
+        '--metrics', '-m', type=str, nargs='*', default=metrics.iterkeys(),
         choices=metrics.keys(),
-        help="Spike train distances to profile. Defaults to all available " +
-        "distances.")
+        help="Spike train metrics to profile. Defaults to all available " +
+        "metrics.")
     parser.add_argument(
-        '--list-distances', '-l', action='store_const', default=False,
+        '--list-metrics', '-l', action='store_const', default=False,
         const=True,
-        help="Print a list of spike train distance which can be used with " +
-        "the -d option.")
-    parser.add_argument(
-        '--output', '-o', type=str, nargs=1,
-        help="Output file for the profiling information.")
+        help="Print a list of spike train metrics which can be used with " +
+        "the -m option.")
     args = parser.parse_args()
 
-    if args.list_distances:
+    if args.list_metrics:
         print_available_metrics()
         sys.exit(0)
 
     try:
-        with open(args.trains[0], 'r') as f:
-            trains = pickle.load(f)
-        print "Loaded stored trains."
+        with open(args.data[0], 'r') as f:
+            data = pickle.load(f)
+        print "Loaded stored benchmarking data."
     except:
-        trains = [stg.gen_homogeneous_poisson(50.0 * pq.Hz, t_stop=4.0 * pq.s)
-                  for i in xrange(6)]
-        if args.trains is not None:
-            with open(args.trains[0], 'w') as f:
-                pickle.dump(trains, f)
-            print "Stored trains."
+        data = BenchmarkData(sp.arange(1, 200, 10), sp.arange(10))
+        if args.data is not None:
+            with open(args.data[0], 'w') as f:
+                pickle.dump(data, f)
+            print "Stored benchmarking data."
 
-    if args.output is None:
-        dummy, out_file = tempfile.mkstemp()
-    else:
-        out_file = args.output[0]
-
-    cProfile.run('profile_metrics(trains, args.distances)', out_file)
-    print_summary(out_file)
-
-    if args.output is None:
-        os.unlink(out_file)
+    b = Benchmark(data, 2)
+    plt.figure()
+    plot_benchmark(b.benchmark_metric(metrics['vr'][1]), data)
+    plt.show()
