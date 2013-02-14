b4b9d55d69f20ef96f11102ec1bf2158f5ccc181
Jan Gosmann <jan@hyper-world.de>
2013-02-14 12:05:38 +0100

    Precompute complete gram matrix.

--- test.py
+++ test.py
@@ -11,18 +11,21 @@ import sklearn.grid_search
 import spykeutils.spike_train_metrics as stm
 
 
-class SpikeTrainMetricApplier(
+class PrecomputedSpikeTrainMetricApplier(
         sklearn.base.BaseEstimator, sklearn.base.TransformerMixin):
-    def __init__(self, metric=stm.van_rossum_dist, tau=1.0 * pq.s):
+    def __init__(self, data, metric=stm.van_rossum_dist, tau=1.0 * pq.s):
         self.metric = metric
         self.tau = tau
+        self.data = data
+        self.x_in = sp.arange(len(data))
 
     def fit(self, x, y):
-        self.x_train = list(x)
+        self.gram = self.metric(self.data, self.tau)
+        self.x_train = x
         return self
 
     def transform(self, x):
-        return self.metric(list(x) + self.x_train, self.tau)[:len(x), len(x):]
+        return self.gram[sp.meshgrid(self.x_train, x)]
 
 
 class SvmClassifierPlugin(analysis_plugin.AnalysisPlugin):
@@ -36,11 +39,13 @@ class SvmClassifierPlugin(analysis_plugin.AnalysisPlugin):
             *([i] * len(selection.spike_trains())
               for i, selection in enumerate(selections)))))
 
-        pipe = Pipeline(steps=[('metric', SpikeTrainMetricApplier()),
-                        ('svm', svm.SVC(kernel='precomputed'))])
+        metricApplier = PrecomputedSpikeTrainMetricApplier(trains)
+        pipe = Pipeline(steps=[
+            ('metric', metricApplier),
+            ('svm', svm.SVC(kernel='precomputed'))])
 
         param_grid = [{'svm__C': [0.1, 0.5, 1, 10, 100, 1000]}]
 
         clf = GridSearchCV(pipe, param_grid)
-        clf.fit(trains, targets)
+        clf.fit(metricApplier.x_in, targets)
         print clf.best_score_, clf.best_estimator_.get_params()['svm__C']
