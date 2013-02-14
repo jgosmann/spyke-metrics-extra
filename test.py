61ff26043ff268cf36a15675d029a4488fa7a368
Jan Gosmann <jan@hyper-world.de>
2013-02-14 11:32:35 +0100

    Working grid search.

--- test.py
+++ test.py
@@ -1,23 +1,28 @@
 from sklearn import svm
+from sklearn.grid_search import GridSearchCV
+from sklearn.pipeline import Pipeline
 from spykeutils.plugin import analysis_plugin
 import itertools
 import quantities as pq
+import scipy as sp
+import sklearn.base
+import sklearn.cross_validation
+import sklearn.grid_search
 import spykeutils.spike_train_metrics as stm
 
 
-class SvmSpikeTrainClassifier(svm.SVC):
-    def __init__(self, metric, tau, **kwargs):
-        svm.SVC.__init__(self, kernel='precomputed', **kwargs)
+class SpikeTrainMetricApplier(
+        sklearn.base.BaseEstimator, sklearn.base.TransformerMixin):
+    def __init__(self, metric=stm.van_rossum_dist, tau=1.0 * pq.s):
         self.metric = metric
         self.tau = tau
 
     def fit(self, x, y):
-        gram = self.metric(x, self.tau)
-        return svm.SVC.fit(self, gram, y)
+        self.x_train = list(x)
+        return self
 
-    def predict(self, x):
-        gram = self.metric(x, self.tau)
-        return svm.SVC.predict(self, gram)
+    def transform(self, x):
+        return self.metric(list(x) + self.x_train, self.tau)[:len(x), len(x):]
 
 
 class SvmClassifierPlugin(analysis_plugin.AnalysisPlugin):
@@ -27,10 +32,15 @@ class SvmClassifierPlugin(analysis_plugin.AnalysisPlugin):
     def start(self, current, selections):
         trains = list(itertools.chain(
             *(selection.spike_trains() for selection in selections)))
-        targets = list(itertools.chain(
+        targets = sp.array(list(itertools.chain(
             *([i] * len(selection.spike_trains())
-              for i, selection in enumerate(selections))))
+              for i, selection in enumerate(selections)))))
 
-        clf = SvmSpikeTrainClassifier(stm.van_rossum_dist, 1.0 * pq.s)
+        pipe = Pipeline(steps=[('metric', SpikeTrainMetricApplier()),
+                        ('svm', svm.SVC(kernel='precomputed'))])
+
+        param_grid = [{'svm__C': [0.1, 0.5, 1, 10, 100, 1000]}]
+
+        clf = GridSearchCV(pipe, param_grid)
         clf.fit(trains, targets)
-        print clf.predict(trains)
+        print clf.best_score_, clf.best_estimator_.get_params()['svm__C']
