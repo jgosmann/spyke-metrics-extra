9699a15fa503d59af3c5d9587e7f174beb3f8965
Jan Gosmann <jan@hyper-world.de>
2013-02-14 13:11:04 +0100

    Create a nice plot.

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from sklearn.grid_search import GridSearchCV
 from sklearn.pipeline import Pipeline
 from spykeutils.plugin import analysis_plugin
 import itertools
+import matplotlib.pyplot as plt
 import quantities as pq
 import scipy as sp
 import sklearn.base
@@ -44,8 +45,32 @@ class SvmClassifierPlugin(analysis_plugin.AnalysisPlugin):
             ('metric', metricApplier),
             ('svm', svm.SVC(kernel='precomputed'))])
 
-        param_grid = [{'svm__C': [0.1, 0.5, 1, 10, 100, 1000]}]
+        param_grid = {
+            'metric__tau': [1, 2, 5, 10, 100] * pq.ms,
+            'svm__C': [0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000]}
 
-        clf = GridSearchCV(pipe, param_grid)
+        clf = GridSearchCV(pipe, param_grid, verbose=1)
         clf.fit(metricApplier.x_in, targets)
-        print clf.best_score_, clf.best_estimator_.get_params()['svm__C']
+        print clf.best_score_, clf.best_estimator_.get_params()
+        self.plot_gridsearch_scores(
+            clf.grid_scores_, param_grid, ('svm__C', 'metric__tau'))
+
+    def plot_gridsearch_scores(
+            self, grid_scores, param_grid, (x_param, y_param)):
+        x, y = sp.meshgrid(param_grid[y_param], param_grid[x_param])
+        counts = sp.zeros_like(x)
+        scores = sp.zeros_like(x)
+
+        for params, s, unused in grid_scores:
+            i = sp.searchsorted(param_grid[x_param], params[x_param])
+            j = sp.searchsorted(param_grid[y_param], params[y_param])
+            counts[i, j] += 1
+            scores[i, j] += s
+        scores /= counts
+
+        plt.contourf(x, y, scores)
+        plt.loglog()
+        plt.xlabel(x_param)
+        plt.ylabel(y_param)
+        plt.colorbar()
+        plt.show()
